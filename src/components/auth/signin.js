23048b68010b7bd798b932da39b1cba470d96422
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-02 15:48:46 -0500

    created titlr page component and styled it.

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
+import PageTitle from '../pageTitle';
 
  class SignIn extends Component {
 
@@ -11,7 +12,8 @@ import SignInForm from './signinForm';
   render() {
     return (
       <div className='sign-in'>
-      < SignInForm onSubmit={this.onSubmit} className='sign-in__form'/>
+        <PageTitle className='sign-in__page-title' title='Login' />
+        < SignInForm onSubmit={this.onSubmit} className='sign-in__form'/>
       </div>
     )
   }
