5d6f4795d8a16ef2db8c4631df8c23ecdd8f0317
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-01 21:34:17 -0500

    built sign in and sign up routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+ class Signin extends Component {
+  render() {
+    return (
+      <div className='signin'>
+       Sign In
+      </div>
+    );
+  }
+}
+
+
+export default Signin;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+ class Signup extends Component {
+  render() {
+    return (
+      <div className='signup'>
+       Sign Up
+      </div>
+    );
+  }
+}
+
+
+export default Signup;
