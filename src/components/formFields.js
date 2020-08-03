1e2089c0328a76eac779db3ee05c743a543e32af
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-03 13:48:09 -0500

    developed sign up form and modified form button

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export class FormInput extends Component {
     render(){
-        const{ className, title, type, placeholder, input } = this.props;
+        const { className, title, type, placeholder, input} = this.props;
         return(
             <div className={`${className} form-input`}>
                 <label className='form-input__label'>{title}</label>
@@ -14,10 +14,10 @@ export class FormInput extends Component {
 
 export class FormButton extends Component {
     render() {
-        const {className, title, type, onClick, input} = this.props;
+        const {className, title, type, onClick, input, short} = this.props;
         return(
             <div className={`${className} form-button`}>
-                <button className={`form-button__button`}
+                <button className={`form-button__button ${short ? 'form-button__gray-button' : ''}`}
                 type={type}
                 {...input}
                 onClick={onClick}
