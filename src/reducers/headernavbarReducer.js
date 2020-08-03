a0e84a0e7a8c9788ef94ed0ccb6d966750f27ab5
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-03 15:17:02 -0500

    built headernavbar reducer

---
+++ src/reducers/headernavbarReducer.js
@@ -0,0 +1,25 @@
+import {
+    SET_HEADER_LINKS, 
+    SET_NAVBAR_LINKS
+} from '../actions/types';
+
+const INITIAL_STATE = {
+    headerLinks: [],
+    navbarLink: []
+}
+
+export default function(state = INITIAL_STATE, action ) {
+    switch (action.type) {
+        case SET_HEADER_LINKS:
+            return {
+                ...state,
+                headerLinks: action.payload
+            }
+            case SET_NAVBAR_LINKS:
+                return {
+                    ...state,
+                    navbarLink: action.payload
+                }
+            default: return state;
+    }
+}
