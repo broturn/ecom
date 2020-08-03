8556ab49a4a4003906deb3a6b71d66d8c083a5e9
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-03 17:23:56 -0500

    styled header and navbar components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
 import headerNavbar from './headernavbarReducer';
+
 const rootReducer = combineReducers({
   form,
   headerNavbar
