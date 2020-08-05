88020a893922b65a88e55cd61687c26b47c64d8e
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-05 09:52:34 -0500

    built long gray button component and placed instance of it on account information form to create change password button

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -8,11 +8,12 @@ class AccountInformation extends Component {
     onSubmit = (fields) => {
         console.log(fields);
     }
+    
     render() {
         return (
-            <div className='account-Information'>
+            <div className='account-information'>
                 <PageTitle className = 'account-information' title='Account Information'/>
-                <AccountInformationForm onSubmit={this.onSubmit} class='account-information__form'/>
+                <AccountInformationForm onSubmit={this.onSubmit} className='account-information__form'/>
             </div>
         )
     }
